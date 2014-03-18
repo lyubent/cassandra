0ecdf5025a3ab596d78be17a75499b8facfdb098
lyubent <ltodorov@dundee.ac.uk>
2014-03-18 02:33:34 +0000

    Change SS#removeSnapshot to static, and switch to using File.list() on the data directories to find keyspaces with snapshots

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -2260,22 +2260,25 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     {
         if(tag == null)
             tag = "";
+        System.out.println("tag: " + tag);
+        System.out.println("ksnames.len" + keyspaceNames.length);
+        for (String s : keyspaceNames)
+            System.out.println("ksnames: " + s);
 
-        Iterable<Keyspace> keyspaces;
-        if (keyspaceNames.length == 0)
+        Set<String> keyspaces = new HashSet<>();
+        for (String dataDir : DatabaseDescriptor.getAllDataFileLocations())
         {
-            keyspaces = Keyspace.all();
-        }
-        else
-        {
-            ArrayList<Keyspace> tempKeyspaces = new ArrayList<Keyspace>(keyspaceNames.length);
-            for(String keyspaceName : keyspaceNames)
-                tempKeyspaces.add(getValidKeyspace(keyspaceName));
-            keyspaces = tempKeyspaces;
+            for(String keyspaceDir : new File(dataDir).list())
+            {
+                // don't add ks if a specific ks has been specified and doesn't match the current ks.
+                if (keyspaceNames.length > 0 && !Arrays.asList(keyspaceNames).contains(keyspaceDir))
+                    continue;
+                keyspaces.add(keyspaceDir);
+            }
         }
 
-        for (Keyspace keyspace : keyspaces)
-            keyspace.clearSnapshot(tag);
+        for (String keyspace : keyspaces)
+            Keyspace.clearSnapshot(tag, keyspace);
 
         if (logger.isDebugEnabled())
             logger.debug("Cleared out snapshot directories");
